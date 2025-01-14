0390e46ebdd64c09f65984e7883eceb241c39471
Afidhu <afidhumatata81@gmail.com>
2025-01-14 03:55:05 +0300

    first commit

--- SOCIAL_MEDIA/settings.py
+++ SOCIAL_MEDIA/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3nm*djstlhfw7ng&+=m*#b^q)cqgw@62()q%zp)+vebsdyy$y=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
